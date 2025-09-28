6619d2cc5da2964327c8424cedc9d82e967b6d13
actlikemunyao <munyaopapain@gmail.com>
2025-09-28 09:45:27 +0300

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -58,9 +58,9 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Use DATABASE_URL on Render, fallback to local SQLite
 DATABASES = {
     'default': dj_database_url.config(
-        default=f"sqlite:///{os.path.join(BASE_DIR, 'db.sqlite3')}",
+        default=os.getenv("DATABASE_URL"),
         conn_max_age=600,
-        ssl_require=False
+        ssl_require=True
     )
 }
 
